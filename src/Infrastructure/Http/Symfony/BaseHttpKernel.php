3041cd459fbc19b3fb5e007365da0f620d77d67a
vitaliy <zndev@ya.ru>
2023-04-05 23:49:40 +0600

    upd

--- src/Infrastructure/Http/Symfony/BaseHttpKernel.php
+++ src/Infrastructure/Http/Symfony/BaseHttpKernel.php
@@ -4,7 +4,6 @@ namespace Untek\Framework\Http\Infrastructure\Http\Symfony;
 
 use Psr\Container\ContainerInterface;
 use Psr\Log\LoggerInterface;
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -13,22 +12,18 @@ use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
 use Symfony\Component\HttpKernel\EventListener\RouterListener;
 use Symfony\Component\HttpKernel\HttpKernel;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
-use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
-use Symfony\Component\Routing\Loader\PhpFileLoader;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
 use Symfony\Component\Routing\RequestContext;
-use Symfony\Component\Routing\RouteCollection;
-use Untek\Core\Container\Interfaces\ContainerConfiguratorInterface;
 use Untek\Core\Container\Traits\ContainerAttributeTrait;
 
 abstract class BaseHttpKernel extends HttpKernel
 {
-
     use ContainerAttributeTrait;
 
     protected string $context;
+
     protected string $environment;
+
     protected bool $debug;
 
     abstract protected function configureRoutes(): void;
@@ -61,27 +56,9 @@ abstract class BaseHttpKernel extends HttpKernel
         int $type = HttpKernelInterface::MAIN_REQUEST,
         bool $catch = true
     ): Response {
-//        $this->bindHttp($this->container);
         $this->configureRoutes();
         $this->addRouterListener($request);
         return parent::handle($request, $type, $catch);
-    }
-
-    protected function bindHttp(ContainerInterface $container): void
-    {
-        $containerConfigurator = $container->get(ContainerConfiguratorInterface::class);
-        $containerConfigurator->bind(UrlMatcherInterface::class, UrlMatcher::class);
-        $containerConfigurator->singleton(RouteCollection::class, RouteCollection::class);
-        $containerConfigurator->singleton(RequestContext::class, RequestContext::class);
-        $containerConfigurator->singleton(
-            RoutingConfigurator::class,
-            function (ContainerInterface $container) {
-                $routeCollection = $container->get(RouteCollection::class);
-                $fileLocator = new FileLocator();
-                $fileLoader = new PhpFileLoader($fileLocator);
-                return new RoutingConfigurator($routeCollection, $fileLoader, __FILE__, __FILE__);
-            }
-        );
     }
 
     protected function addRouterListener(Request $request): void
@@ -96,10 +73,9 @@ abstract class BaseHttpKernel extends HttpKernel
             $requestStack,
             $requestContext,
             $logger,
-            $this->getProjectDir(),
+            null, //$this->getProjectDir(),
             $this->debug
         );
         $this->dispatcher->addSubscriber($routerListener);
     }
-
 }
